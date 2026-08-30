5e6410841d9ad197acb4d07086eb49d7518b7158
sagirushuaibu999-ship-it <sagirushuaibu999@gmail.com>
2026-08-30 20:21:12 +0100

    Update Server.js

--- Server.js
+++ Server.js
@@ -1,1 +1,20 @@
-Service.js
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    status: "success",
+    message: "Ya Salam Business backend is running"
+  });
+});
+
+const PORT = process.env.PORT || 10000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
